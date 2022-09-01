5a7e33988f73ac62dbb0e119a22026ebe4b6ffef
Salomon Chambi <salomonchambi@gmail.com>
2022-08-31 19:29:17 -0700

    Contain the structure and prototypes of the project

--- binary_trees.h
+++ binary_trees.h
@@ -14,14 +14,18 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
+/* typedef as needed */
+typedef struct binary_tree_s binary_tree_t;
+
+/* print a binary tree */
 void binary_tree_print(const binary_tree_t *);
 
 /* prototypes of the project */
